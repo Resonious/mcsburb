c93fa5b64da7f304ed23cca344832a19d5476a99
Travis <troyald@gmail.com>
2015-04-24 15:01:02 -0400

    nigel save me

---
+++ src/main/scala/net/dinkyman/sburb/Reference.java
@@ -0,0 +1,8 @@
+package net.dinkyman.sburb.help;
+
+public class Reference
+{
+	public static final String MODID = "dreamDimensions";
+	public static final String NAME = "ProspitDerse";
+	public static final String VERSION = "1.0";
+}
